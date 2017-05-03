ee9742a7c8915b2f22e6c91368b4dc1ae61800fa
Tal Abziz <Abziz89@gmail.com>
2017-05-03 14:23:47 +0300

    fix responsiveness

--- app/src/main/java/com/example/abziz/simon/GameActivity.java
+++ app/src/main/java/com/example/abziz/simon/GameActivity.java
@@ -5,22 +5,24 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
 
+import pl.droidsonroids.gif.AnimationListener;
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
 
 public class GameActivity extends AppCompatActivity {
     final Handler hand = new Handler();
     GifImageView[] pokemons;
-    GifDrawable[] animation_info;
     final Random rand = new Random();
-    int[] roarAnimation ;
-    int[] idleAnimation ;
+    GifDrawable[] roarAnimation ;
+    GifDrawable[] idleAnimation ;
     int[] roarSounds;
+    boolean AnimationRunning = false;
     static final int Bulbasaur =0, Charmander =1, Squirtle=2,Pikachu=3;
     ArrayList<Integer> sequence = new ArrayList();
     int current = 0;
@@ -32,15 +34,6 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         AssignViews();
         setListeners();
-        for(int i =0;i<5;i++){
-            NextLevel();
-        }
-
-        try {
-            PlayAll();
-        }catch(Exception e){
-            ShowText(e.getMessage());
-        }
     }
 
     private void NextLevel(){
@@ -63,24 +56,24 @@ public class GameActivity extends AppCompatActivity {
                     if( current != 0){
                         int last = sequence.get(current-1);
                         pokemons[last].setPressed(false);
-                        pokemons[last].setImageResource(idleAnimation[last]);
+                        pokemons[last].setImageDrawable(idleAnimation[last]);
                     }
-                    pokemons[pokemon].setImageResource(roarAnimation[pokemon]);
+                    pokemons[pokemon].setImageDrawable(roarAnimation[pokemon]);
                     pokemons[pokemon].setPressed(true);
                     if(current < sequence.size()-1) {
                         current++;
-                        hand.postDelayed(this, animation_info[pokemon].getDuration());
+                        hand.postDelayed(this, roarAnimation[pokemon].getDuration());
                     }else{
                         int last = sequence.get(sequence.size()-1);
                         pokemons[last].setPressed(false);
-                        pokemons[last].setImageResource(idleAnimation[last]);
+                        pokemons[last].setImageDrawable(idleAnimation[last]);
                         for( GifImageView v : pokemons){
                             v.setEnabled(true);
                         }
                     }
             }
         }
-        ,animation_info[sequence.get(0)].getDuration());
+        ,roarAnimation[sequence.get(0)].getDuration());
 
 
     }
@@ -88,30 +81,55 @@ public class GameActivity extends AppCompatActivity {
     private void AssignViews(){
         try {
             pokemons = new GifImageView[4];
-            roarAnimation = new int[4];
-            idleAnimation = new int[4];
+            roarAnimation = new GifDrawable[4];
+            idleAnimation = new GifDrawable[4];
             roarSounds = new int[4];
-            animation_info = new GifDrawable[4];
 
             pokemons[Pikachu] = (GifImageView) findViewById(R.id.pikachu);
-            idleAnimation[Pikachu] = R.drawable.pikachu_idle;
-            roarAnimation[Pikachu] = R.drawable.pikachu_roar;
-            animation_info[Pikachu] = new GifDrawable( getResources(), roarAnimation[Pikachu]);
+            idleAnimation[Pikachu] = new GifDrawable( getResources(), R.drawable.pikachu_idle);
+            roarAnimation[Pikachu] = new GifDrawable( getResources(), R.drawable.pikachu_roar);;
+            roarAnimation[Pikachu].addAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationCompleted(int loopNumber) {
+                    pokemons[Pikachu].setPressed(false);
+                    pokemons[Pikachu].setImageDrawable(idleAnimation[Pikachu]);
+                }
+            });
 
             pokemons[Bulbasaur] = (GifImageView) findViewById(R.id.bulbasaur);
-            idleAnimation[Bulbasaur] = R.drawable.bulbasaur_idle;
-            roarAnimation[Bulbasaur] = R.drawable.bulbasaur_roar;
-            animation_info[Bulbasaur] = new GifDrawable( getResources(), roarAnimation[Bulbasaur]);
+            idleAnimation[Bulbasaur] = new GifDrawable( getResources(),R.drawable.bulbasaur_idle);
+            roarAnimation[Bulbasaur] = new GifDrawable( getResources(),R.drawable.bulbasaur_roar);
+            roarAnimation[Bulbasaur].addAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationCompleted(int loopNumber) {
+                    pokemons[Bulbasaur].setPressed(false);
+                    pokemons[Bulbasaur].setImageDrawable(idleAnimation[Bulbasaur]);
+                }
+            });
+
 
             pokemons[Charmander] = (GifImageView) findViewById(R.id.charmander);
-            idleAnimation[Charmander] = R.drawable.charmander_idle;
-            roarAnimation[Charmander] = R.drawable.charmander_roar;
-            animation_info[Charmander] = new GifDrawable( getResources(), roarAnimation[Charmander]);
+            idleAnimation[Charmander] = new GifDrawable( getResources(),R.drawable.charmander_idle);
+            roarAnimation[Charmander] = new GifDrawable( getResources(),R.drawable.charmander_roar);
+            roarAnimation[Charmander].addAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationCompleted(int loopNumber) {
+                    pokemons[Charmander].setPressed(false);
+                    pokemons[Charmander].setImageDrawable(idleAnimation[Charmander]);
+                }
+            });
 
             pokemons[Squirtle] = (GifImageView) findViewById(R.id.squirtle);
-            idleAnimation[Squirtle] = R.drawable.squirtle_idle;
-            roarAnimation[Squirtle] = R.drawable.squirtle_roar;
-            animation_info[Squirtle] = new GifDrawable( getResources(), roarAnimation[Squirtle]);
+            idleAnimation[Squirtle] = new GifDrawable( getResources(),R.drawable.squirtle_idle);
+            roarAnimation[Squirtle] = new GifDrawable( getResources(),R.drawable.squirtle_roar);
+            roarAnimation[Squirtle].addAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationCompleted(int loopNumber) {
+                    pokemons[Squirtle].setPressed(false);
+                    pokemons[Squirtle].setImageDrawable(idleAnimation[Squirtle]);
+                }
+            });
+
 
 
         }catch (Exception e){
@@ -165,15 +183,10 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void act(final int pokemon){
-        pokemons[pokemon].setImageResource(roarAnimation[pokemon]);
+        roarAnimation[pokemon].reset();
+        roarAnimation[pokemon].setLoopCount(1);
         pokemons[pokemon].setPressed(true);
-        hand.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                pokemons[pokemon].setPressed(false);
-                pokemons[pokemon].setImageResource(idleAnimation[pokemon]);
-            }
-        },animation_info[pokemon].getDuration());
+        pokemons[pokemon].setImageDrawable(roarAnimation[pokemon]);
     }
 }
 
